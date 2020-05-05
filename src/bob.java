b44e965bdb3fcb24d90f27f605187a66b4dc0b9b
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-05 10:18:04 -0500

    start class bob

---
+++ src/bob.java
@@ -0,0 +1,14 @@
+public class bob {
+    public static void main (String[] args){
+
+
+        //2. Create a class named Bob with a main method for the following exercise.
+        //Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+        //Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+        //He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+        //He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+        //He answers 'Whatever.' to anything else.
+        //Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+    }
+
+}
