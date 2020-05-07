ee08646d4eb3f4698c32290b6ab85af7c82ae95e
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-07 13:47:13 -0500

    post lecture

--- src/oop/PersonTest.java
+++ src/oop/PersonTest.java
@@ -2,14 +2,35 @@ package oop;
 
 public class PersonTest {
     public static void main(String[] args) {
-        personLecture nayoung= new personLecture();
-        personLecture ana = new personLecture();
-        nayoung.firstName = "Nayoung";
-        nayoung.lastName = "kim";
+        //creating 2 instances for each case.
+//        personLecture nayoung= new personLecture();
+//        personLecture ana = new personLecture();
+//        personLecture deadPerson = new personLecture();
 
-        ana.firstName="Ana";
-        ana.lastName= "Plant";
+        //assigning values to the fields
+//        nayoung.firstName = "Nayoung";
+//        nayoung.lastName = "kim";
+//        nayoung.isAlive = true;
+//
+//        System.out.println("Person. = " + personLecture.race);
+//
+//        ana.firstName="Ana";
+//        ana.lastName= "Plant";
+//        ana.isAlive = true;
+//        deadPerson.isAlive = false;
+       // nayoung.person  since you made the firstName private, the user cannot change the first name
+
+
+        //being specific in the constructor to set rules.
+        personLecture nayoung = new personLecture ("Nayoung", "Kim", true);
+        personLecture ana = new personLecture("Ana", "Plant", true);
+        personLecture deadPerson = new personLecture(null, null, false);
+
+
+        System.out.println("personLecture.race= " + personLecture.race);
         System.out.println("nayoung.sayHello()= " + nayoung.sayHello());
         System.out.println("ana.sayHello() = " + ana.sayHello());
+        System.out.println("deadPerson.sayHello() = " + deadPerson.sayHello());
+
     }
 }
--- src/oop/personLecture.java
+++ src/oop/personLecture.java
@@ -1,12 +1,36 @@
 package oop;
 
 public class personLecture {
-    //creating properties in the class instead of the main method. This is the "global" scope.
-    public String firstName;
-    public String lastName;
 
-    public String sayHello(){
-        return String.format ("Hello from %s %s!", firstName, lastName);
+    public static String race = "HUMAN RACE";
+
+
+    //creating properties in the class instead of the main method. This is the "global" scope.
+    private String firstName;
+    private String lastName;
+    protected boolean isAlive;
+
+    //creating rules in order to create an instance
+    // setting rules: everyone should have a first name, last name, and a value to know if they are alive
+    public personLecture(String fName, String lName, boolean alive) {
+        //using the this keyword will keep things organized (what is this vs what is the argument)
+        this.firstName = fName;
+        this.lastName = lName;
+        this.isAlive = alive;
     }
 
+
+    // object{
+//    number : 1
+//    function() {
+//        this.number
+//    }
+    //}
+    public String sayHello() {
+        if (this.isAlive) {
+            return String.format("Hello from %s %s!", this.firstName, this.lastName);
+        } else {
+            return "This person is not alive anymore :(";
+        }
+    }
 }
