e6791063113a0cacb527a60e390c01b2c0618047
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-05-07 22:44:21 -0500

    number 1-redo afterclass

---
+++ src/oop/Husband.java
@@ -0,0 +1,22 @@
+package oop;
+
+public class Edward {
+
+    //field
+    private String name;
+    private String occupation;
+    private String birthday;
+    private String occupation;
+    private byte age;
+    private boolean male;
+
+    //constructor
+    public Edward (String name, String occupation, byte age){
+        this.name = name;
+        this.occupation = occupation;
+        this.age = age;
+    }
+
+
+
+}
