cd2fbda364bbde5609a6fb4ede48c113d56098be
nayokim <nayoung.kimpak@gmail.com>
2020-08-31 15:32:43 -0500

    rephrase the question

---
+++ src/DataStructures/StringToInteger.java
@@ -0,0 +1,16 @@
+package DataStructures;
+
+public class StringToInteger {
+
+
+    //Given a String, return its appropriate number value
+    //ignore all white-space at the beginning of the string
+    //number may begin with a negative or positive
+    // all characters that come after the number should be ignored
+    // String is invalid if a character that is not a white-space or sign comes before the number.
+    // If string does not contain any integer values, it is invalid.
+    // The return value for any invalid string is 0.
+    // Resulting integer cannot be larger than (2^31) — 1 or smaller than -(2^31).
+
+
+}
